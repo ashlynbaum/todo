e03da2fdf3e28e56ad7be0392595e83d1eca1926
Ashlyn Baum <a@ashlynbaum.com>
2015-05-31 12:10:57 +1200

    Added tests for creating todo items and associated code

---
+++ spec/features/todo_items/create_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+describe "Create todo items" do
+  let!(:todo_list) { TodoList.create(title: "Grocery List", description: "Groceries")}
+
+  def visit_todo_list(list)
+    visit "/todo_lists"
+    within "#todo_list_#{list.id}" do
+      click_link "List Items"
+    end
+  end
+
+  it "is successful with valid content" do
+    visit_todo_list(todo_list)
+    click_link "New Todo Item"
+    fill_in "Content", with: "Avocados"
+    click_button "Save"
+    expect(page).to have_content("Added todo list item.")
+    within("ul.todo_items") do
+      expect(page).to have_content("Avocados")
+    end
+  end
+
+  it "displays an error with no content" do
+    visit_todo_list(todo_list)
+    click_link "New Todo Items"
+    fill_in "Content" :with ""
+    click_button "Save"
+    within("div.flash") do
+      expect(page).to have_content("There was a problem adding that todo list item.")
+    end
+    epect(page).to have_content("Content can't be blank")
+  end
+
+end
